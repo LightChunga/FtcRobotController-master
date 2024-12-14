deed78253fb85fe319c1121e77c4dcad5b77a3fb
LightChunga <stefanciucanu1@gmail.com>
2024-12-14 12:00:03 +0200

    ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/New/AutoRedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/New/AutoRedLeft.java
@@ -98,10 +98,10 @@ public class AutoRedLeft extends LinearOpMode {
 
         //ToDo
         TrajectorySequence towall = drive.trajectorySequenceBuilder(new Pose2d(-24, -60.00, Math.toRadians(270.00)))
-                .lineToConstantHeading(new Vector2d(-8.0, -38.40))
+                .lineToConstantHeading(new Vector2d(-8.0, -36.40))
                 .build();
 
-        TrajectorySequence firstcube = drive.trajectorySequenceBuilder(new Pose2d(-8.00, -38.40, Math.toRadians(270.00)))
+        TrajectorySequence firstcube = drive.trajectorySequenceBuilder(new Pose2d(-8.00, -36.40, Math.toRadians(270.00)))
                 .splineTo(new Vector2d(-37.77, -40.80), Math.toRadians(90.00))
                 .splineTo(new Vector2d(-47.45, -9.75), Math.toRadians(180.00))
                 .lineTo(new Vector2d(-48, -60))
