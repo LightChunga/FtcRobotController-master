ab8c38350d342454bb1db933305806337a680970
LightChunga <stefanciucanu1@gmail.com>
2024-12-19 11:17:02 +0200

    ok

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/New/AutoRedLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/New/AutoRedLeft.java
@@ -9,6 +9,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
@@ -41,6 +42,46 @@ public class AutoRedLeft extends LinearOpMode {
         sliderright.setPower(pow);
     }
 
+    public void RaiseArm(double h0, Telemetry telemetry) {
+        double dif_h = rp * h0 - L0;
+
+        int pts = (int)pcm * (int)dif_h;
+
+        //double pwr = 0;
+
+        sliderright.setPower(-0.9);
+        sliderleft.setPower(-0.9);
+
+        while(-sliderright.getCurrentPosition() <= pts) {
+            telemetry.addData("Current Encoder Position: ", -sliderright.getCurrentPosition());
+            telemetry.addData("Pts: ", pts);
+            telemetry.update();
+        }
+
+        sliderright.setPower(0);
+        sliderleft.setPower(0);
+    }
+
+    public void LowerArm(double h0, Telemetry telemetry) {
+        double dif_h = rp * h0 - L0;
+
+        int pts = (int)pcm * (int)dif_h;
+
+        //double pwr = 0;
+
+        sliderright.setPower(0.9);
+        sliderleft.setPower(0.9);
+
+        while(-sliderright.getCurrentPosition() >= pts) {
+            telemetry.addData("Current Encoder Position: ", -sliderright.getCurrentPosition());
+            telemetry.addData("Pts: ", pts);
+            telemetry.update();
+        }
+
+        sliderright.setPower(0);
+        sliderleft.setPower(0);
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -66,14 +107,14 @@ public class AutoRedLeft extends LinearOpMode {
 
         //ToDo
         TrajectorySequence towall = drive.trajectorySequenceBuilder(new Pose2d(-24, -60.00, Math.toRadians(270.00)))
-                .lineToConstantHeading(new Vector2d(-8, -37.80))
+                .lineToConstantHeading(new Vector2d(-8, -41))
                 .build();
 
-        TrajectorySequence tocubes = drive.trajectorySequenceBuilder(new Pose2d(-8.00, -37.8, Math.toRadians(270.00)))
+        TrajectorySequence tocubes = drive.trajectorySequenceBuilder(new Pose2d(-8.00, -41.00, Math.toRadians(270.00)))
                 .splineTo(new Vector2d(-56.11, -43.98), Math.toRadians(70.00))
                 .build();
 
-        TrajectorySequence firstcube = drive.trajectorySequenceBuilder(new Pose2d(-6.00, -38, Math.toRadians(270.00)))
+        TrajectorySequence firstcube = drive.trajectorySequenceBuilder(new Pose2d(-6.00, -40, Math.toRadians(270.00)))
                 .splineTo(new Vector2d(-37.77, -40.80), Math.toRadians(90.00))
                 .splineTo(new Vector2d(-47.45, -9.75), Math.toRadians(180.00))
                 .lineTo(new Vector2d(-48, -60))
@@ -97,16 +138,15 @@ public class AutoRedLeft extends LinearOpMode {
 
         if (isStopRequested()) return;
 
-       drive.setPoseEstimate(towall.start());
+        drive.setPoseEstimate(towall.start());
 
-        setarmheight(80, 1.0);
         drive.followTrajectorySequence(towall);
-        while (sliderright.isBusy() && sliderleft.isBusy()) {}
+        RaiseArm(80, telemetry);
+        /*while (sliderright.isBusy() && sliderleft.isBusy()) {}
         sleep(100);
         servospate.setPosition(0);
-        servobk.setPosition(0);
         setarmheight(29, -1.0);
-        //drive.followTrajectorySequence(tocubes);
-        while (sliderright.isBusy() && sliderleft.isBusy()) {}
+        drive.followTrajectorySequence(tocubes);
+        while (sliderright.isBusy() && sliderleft.isBusy()) {}*/
     }
 }
